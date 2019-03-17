05edd892c867fa52e8f7ec0bbce1b7d00edb874c
ilhamgumay <apanamanya001@gmail.com>
2019-03-18 01:08:22 +0700

    publish file index

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 /*
 |--------------------------------------------------------------------------
 | Web Routes
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -11,11 +20,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
+//Route::get($url,$callback);
 Route::get('/hello',function(){
     return 'Hello World!';
 });
+
+
+Route::get('/welcome', function () {
+echo "Welcome";
+});
+
+Route::get('/index', function () {
+echo "Uji Coba route dengan method GET";
+});
+Route::get('/show/{id}', function ($id) {
+echo "Nilai Parameter Adalah ".$id;
+});
+
+
+Route::get('/show/{id?}', function ($id=1) {
+echo "Nilai Parameter Adalah ".$id;
+});
+
+Route::get('/edit/{nama}', function ($nama) {
+echo "Nilai Parameter Adalah ".$nama;
+})->where('nama','[A-Za-z]+');
